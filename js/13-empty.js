bcf08bc0dbb5de8cb3c1daf9748b2f5370284cee
Ana onablo <79449819+onablo@users.noreply.github.com>
2021-03-16 23:47:24 +0200

    Sandauga

---
+++ js/13-empty.js
@@ -0,0 +1,5 @@
+function tusciaFunkcija() {
+    return false
+}
+console.log(tusciaFunkcija());
+console.log(tusciaFunkcija(552244));
---
+++ js/14-multiply.js
@@ -0,0 +1,17 @@
+console.log('--------------------------')
+
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5)),
+console.log(daugyba(44, 5)),
+console.log(daugyba(15, 15)),
+console.log(daugyba(45, 50)),
+console.log(daugyba(-45, -15)),
+console.log(daugyba(400, -5)),
+console.log(daugyba(0, 15)),
+console.log(daugyba(3.14, 12)),
+
+console.log('--------------------------')
