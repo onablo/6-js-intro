f12e028cc7e07f8450612c97e13412d63c64158e
Ana onablo <79449819+onablo@users.noreply.github.com>
2021-03-22 00:30:40 +0200

    BigNum kai sarase gali buti bet kas

--- js/16-bigNum.js
+++ js/16-bigNum.js
@@ -8,6 +8,86 @@ function bigNum(list) {
     }
   
     // logic
+    let biggest = -Infinity;
+
+    for (let i = 0; i < list.length; i++ ) {
+        const number = list[i];
+
+        //ar tai normalus skaicius
+        if (typeof number !== 'number' || !isFinite(number)) {
+            continue;  //eina i sekancia ciklo iteracija
+        }
+
+        //ar jis didesnis uz jau zinoma didziausia skaiciu
+        if (number > biggest) {
+            biggest = number;
+        }
+    }
+
+    //logic validation
+    if (biggest === -Infinity) {
+        return 'ERROR: sarase nerastas nei vienas normalus skaicius';
+    }
+
+    // result
+    return biggest;
+}
+
+
+console.log(bigNum([1]), '->', 1);
+console.log(bigNum([1, 2, 3]), '->', 3);
+console.log(bigNum([-5, 78, 14, 0, 18]), '->', 78);
+console.log(bigNum([69, 69, 69, 69, 66]), '->', 69);
+console.log(bigNum([-1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
+console.log(bigNum([-15, -32, -3, -34, -5, -6, -7, -8]), '->', -3);
+
+console.log(bigNum([1, 8, 3, 5, Infinity]), '->', 8);
+console.log(bigNum([1, 8, Infinity, 3, 5]), '->', 8);
+console.log(bigNum([Infinity, 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, 'pomidoras']), '->', 8);
+console.log(bigNum([1, 8, 'pomidoras', 3, 5]), '->', 8);
+console.log(bigNum(['pomidoras', 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, true]), '->', 8);
+console.log(bigNum([1, 8, true, 3, 5]), '->', 8);
+console.log(bigNum([true, 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, null]), '->', 8);
+console.log(bigNum([1, 8, null, 3, 5]), '->', 8);
+console.log(bigNum([null, 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, undefined]), '->', 8);
+console.log(bigNum([1, 8, undefined, 3, 5]), '->', 8);
+console.log(bigNum([undefined, 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, NaN]), '->', 8);
+console.log(bigNum([1, 8, NaN, 3, 5]), '->', 8);
+console.log(bigNum([NaN, 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([1, 8, 3, 5, '']), '->', 8);
+console.log(bigNum([1, 8, '', 3, 5]), '->', 8);
+console.log(bigNum(['', 1, 8, 3, 5]), '->', 8);
+
+console.log(bigNum([Infinity, null, 'sads', true, 888]), '->', 888);
+console.log(bigNum([Infinity, null, 888, 'sads', true]), '->', 888);
+console.log(bigNum([888, Infinity, null, 'sads', true]), '->', 888);
+
+console.log(bigNum(['888', Infinity, null, 'sads', true]));
+
+
+console.log('---------------');
+
+/*function bigNum(list) {
+    // input validation
+    if (!Array.isArray(list)) {
+        return 'ERROR: netinkamas tipas, turi buti array (sarasas)';
+    }
+    if (list.length === 0) {
+        return 'Error: array is empty';
+    }
+  
+    // logic
     let biggest = list[0];
 
     for (let i = 1; i < list.length; i++ ) {
@@ -38,6 +118,6 @@ console.log(bigNum([-5, 78, 14, 0, 18]), '->', 78);
 console.log(bigNum([69, 69, 69, 69, 66]), '->', 69);
 console.log(bigNum([-1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
 console.log(bigNum([-15, -32, -3, -34, -5, -6, -7, -8]), '->', -3);
-
+*/
 
  
